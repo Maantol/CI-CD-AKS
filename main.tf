7d8034d0f26a3a09e0b370cd74e6f6ccb0176eb9
Matti Tolonen <mattitolonen@Matti-Mac-mini.local>
2025-10-13 19:52:44 +0300

    modification to env

--- main.tf
+++ main.tf
@@ -6,20 +6,35 @@ provider "azurerm" {
   }
 }
 
+provider "random" {}
+
+resource "random_pet" "rg_name" {
+  length    = 2
+  separator = "-"
+}
+
+resource "random_pet" "network_name" {
+  length = 1
+}
+
+resource "random_pet" "subnet_name" {
+  length = 1
+}
+
 resource "azurerm_resource_group" "example" {
-  name     = "exampleTFResourceGroup"
+  name     = "rg-${random_pet.rg_name.id}"
   location = var.azure_location
 }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
+  name                = "${random_pet.network_name.id}-network"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
 
 resource "azurerm_subnet" "example" {
-  name                 = "internal"
+  name                 = "${random_pet.subnet_name.id}-subnet"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
--- terraform.tf
+++ terraform.tf
@@ -4,7 +4,21 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "4.47.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.7.2"
+    }
   }
   cloud {
-    }
+  }
 }
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+provider "random" {}
