7a18a354d37d4f5beb4356a16ae3b9545ed08442
Matti Tolonen <mattitolonen@Matti-Mac-mini.local>
2025-10-13 19:54:14 +0300

    missed terraform file save

--- terraform.tf
+++ terraform.tf
@@ -12,13 +12,3 @@ terraform {
   cloud {
   }
 }
-
-provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
-  }
-}
-
-provider "random" {}
