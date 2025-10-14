bcabf9e18c36d215b7ed2cac74b3a93e1bf9b2ff
Matti Tolonen <mattitolonen@Matti-Mac-mini.local>
2025-10-14 19:19:09 +0300

    missed sensitive value and pre-plan check

--- outputs.tf
+++ outputs.tf
@@ -34,7 +34,7 @@ output "cluster_username" {
 
 output "host" {
   value     = azurerm_kubernetes_cluster.k8s.kube_config[0].host
-  sensitive = false
+  sensitive = true
 }
 output "kube_config" {
   value     = azurerm_kubernetes_cluster.k8s.kube_config_raw
